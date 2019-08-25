ef9a18e719d37189c4bb63b8b7b9e438ae8c96e4
YJChan <yjtoro@hotmail.com>
2019-08-26 00:42:49 +0800

    try on writing test case

--- backend/src/base/entities/login-type/login-type.service.spec.ts
+++ backend/src/base/entities/login-type/login-type.service.spec.ts
@@ -1,18 +1,75 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { LoginTypeService } from './login-type.service';
+import { LoginTypeDTO } from './dto/login-type.dto';
+import { Repository, getRepository, getConnection } from 'typeorm';
+import { LoginTypeEntity } from './login-type.entity';
+import { UserAuthEntity } from '../user-auth/user-auth.entity';
+import { UserEntity } from '../user/user.entity';
+import { RoleEntity } from '../role/role.entity';
+import { UserAuthService } from '../user-auth/user-auth.service';
+import { UserService } from '../user/user.service';
+import { RoleService } from '../role/role.service';
+import {createConnection, Connection} from 'typeorm';
 
 describe('LoginTypeService', () => {
   let service: LoginTypeService;
+  let loginTypeDTO: LoginTypeDTO;
+  const loginTypeRepository: Repository<LoginTypeEntity> = new Repository<LoginTypeEntity>();
+  const userAuthRepository: Repository<UserAuthEntity> = new Repository<UserAuthEntity>();
+  const userRepository: Repository<UserEntity> = new Repository<UserEntity>();
+  const roleRepository: Repository<RoleEntity> = new Repository<RoleEntity>();
+  const userService: UserService = new UserService(userRepository, userAuthRepository);
+  const roleService: RoleService = new RoleService(roleRepository, userAuthRepository);
+  const userAuthService: UserAuthService = new UserAuthService(userAuthRepository, userService, roleService);
+  let loginTypeRepo;
+  beforeAll(async () => {
+    const connection = await createConnection({
+      name: 'test_default',
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      entities: [LoginTypeEntity],
+    });
+  });
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LoginTypeService],
-    }).compile();
+    // const module: TestingModule = await Test.createTestingModule({
+    //   imports: [TypeOrmModule.forFeature([LoginTypeEntity]), forwardRef(() => UserAuthModule)],
+    //   providers: [
+    //     LoginTypeService,
+    //   ],
+    // }).compile();
 
-    service = module.get<LoginTypeService>(LoginTypeService);
+    loginTypeRepo = getConnection('test_default').getRepository(LoginTypeEntity);
+    service = new LoginTypeService(loginTypeRepo, userAuthService);
+    loginTypeDTO = new LoginTypeDTO();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be get all login type', async () => {
+    try {
+      const result = await service.findAll();
+      expect(result.length).toBeGreaterThan(0);
+    } catch (err) {
+      // console.log('cannot find any login type ', err);
+      expect(err).toEqual('login type not created');
+    }
+  });
+
+  it('should create new login type', async () => {
+    try {
+      loginTypeDTO = new LoginTypeDTO();
+      loginTypeDTO.name = 'testing';
+      const result = await service.create(loginTypeDTO);
+      expect(result.name).toEqual('testing');
+    } catch (err) {
+      expect(err).toEqual('login type not created');
+    }
   });
 });
--- backend/src/base/entities/user/user.service.spec.ts
+++ backend/src/base/entities/user/user.service.spec.ts
@@ -1,5 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserEntity } from './user.entity';
+import { UserDTO } from './dto/user.dto';
+
+jest.mock('./user.service.ts');
+const userMockList: UserEntity[] = [
+  {
+    id: 'b623a864-f3e4-4916-ba31-c94527535bdf',
+    createdDate: new Date(),
+    updatedDate: new Date(),
+    firstName: 'test',
+    lastName: 'test 2',
+    middleName: 't ',
+    email: 'test@test.com',
+    phoneNum: '12345678',
+    status: true,
+    version: 1,
+    userAuth: null,
+  },
+];
 
 describe('UserService', () => {
   let service: UserService;
@@ -14,5 +33,19 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get one user', async () => {
+    try {
+      const userList: Promise<UserEntity[]> = new Promise((resolve, reject) => {
+        resolve(userMockList);
+      });
+      jest.spyOn(service, 'showAll').mockResolvedValue(userList);
+      const users = await service.showAll();
+      expect(userMockList).toEqual(users);
+      expect(users.length).toEqual(1);
+    } catch (err) {
+      expect(err).toBeFalsy();
+    }
   });
 });
